778462d68eabb4b52d46718a2b9f9547a6823f18
Charles Taylor <taylorc822@gmail.com>
2025-02-02 22:09:41 +1100

    feat: add tests for each api branch

--- hCommenter.Api/test/ApiProperties.hs
+++ hCommenter.Api/test/ApiProperties.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
 
 module ApiProperties where
 
@@ -8,7 +9,7 @@ import Data.Maybe (fromJust)
 import Hedgehog
 import Hedgehog.Servant
 import Network.HTTP.Types (Status (statusCode), hCacheControl, methodGet, methodPost, ok200)
-import Network.Wai (Application, Request (requestMethod), mapRequestHeaders)
+import Network.Wai (Application, Request (requestHeaders, requestMethod), mapRequestHeaders)
 import Network.Wai.Test
   ( SRequest (SRequest, simpleRequest)
   , SResponse (simpleHeaders, simpleStatus)
@@ -17,6 +18,8 @@ import Network.Wai.Test
   , setPath
   , withSession
   )
+import Servant.API
+import Servant.API.Verbs
 import Servant.Client
 import Test.Hspec.Hedgehog (hedgehog)
 
@@ -24,41 +27,24 @@ import Data.ByteString.Lazy.Internal qualified as Lazy
 import Hedgehog.Gen qualified as Gen
 import Network.HTTP.Client qualified as Client
 
-import Server (CommentsAPI, HealthAPI, VotingAPI, app, mkEnv)
+import Server (FunctionalAPI, app, mkEnv)
 import Utils.Generators (genCommentKey, genKey, genNewComment, genSortBy, genText)
 
-getTestResponse :: PropertyT IO SResponse
-getTestResponse = do
-  req <-
-    forAll $
-      Gen.choice
-        [ genReq (Proxy @CommentsAPI)
-        , genReq (Proxy @HealthAPI)
-        , genReq (Proxy @VotingAPI)
-        ]
+requireApiBestPracticesFor apiProxy makeReq = hedgehog $ do
+  req <- forAll $ genReq apiProxy
+  response <- liftIO (makeReq req)
 
-  -- requestMethod req === methodPost
+  alwaysCacheControlOnGetRequests req response
+  neverRespondWithInternalError response
 
-  testRequest req
-
-alwaysCacheControlOnGetRequests :: SResponse -> PropertyT IO ()
-alwaysCacheControlOnGetRequests response = do
-  diff (hCacheControl, "no-cache") elem (simpleHeaders response)
+alwaysCacheControlOnGetRequests :: Request -> SResponse -> PropertyT IO ()
+alwaysCacheControlOnGetRequests req response = do
+  when (requestMethod req == methodGet) $ do
+    diff (hCacheControl, "no-cache") elem (simpleHeaders response)
 
 neverRespondWithInternalError :: SResponse -> PropertyT IO ()
 neverRespondWithInternalError response = do
-  diff (statusCode (simpleStatus response)) (<) 400
-
-testRequest :: Request -> PropertyT IO SResponse
-testRequest req = do
-  myApp <- liftIO api
-  liftIO $ withSession myApp $ do
-    request req
-
-api :: IO Application
-api = do
-  env <- mkEnv
-  pure $ app env
+  diff (statusCode (simpleStatus response)) (<) 500
 
 genReq apiProxy =
   genRequest apiProxy (genCommentKey :*: genSortBy :*: genText :*: genNewComment :*: GNil)
@@ -66,9 +52,9 @@ genReq apiProxy =
 
 toWaiRequest :: Client.Request -> Request
 toWaiRequest creq =
-  defaultRequest
+  defaultRequest {requestMethod = creqMethod, requestHeaders = creqHeaders}
     & flip setPath creqPath
-    & mapRequestHeaders (mappend creqHeaders)
  where
+  creqMethod = Client.method creq
   creqPath = Client.path creq
   creqHeaders = Client.requestHeaders creq
--- hCommenter.Api/test/Main.hs
+++ hCommenter.Api/test/Main.hs
@@ -3,13 +3,13 @@
 module Main (main) where
 
 import Control.Concurrent (threadDelay)
+import Data.Data (Proxy (Proxy))
 import Hedgehog
 import Hedgehog.Internal.Property (PropertyT (PropertyT))
-import Network.Wai (Application)
+import Network.Wai (Application, Request)
+import Network.Wai.Test (SResponse, Session, request, withSession)
 import Test.Hspec
 import Test.Hspec.Hedgehog (hedgehog, modifyMaxSuccess)
-import Test.Hspec.Wai
-import Test.Hspec.Wai.Internal (runWithState, withApplication)
 
 import Hedgehog.Gen qualified as Gen
 import Hedgehog.Range qualified as Range
@@ -27,14 +27,23 @@ spec = do
   exampleHedghehogSpec
 
 apiPropertySpec :: Spec
-apiPropertySpec = describe "API" $ do
-  modifyMaxSuccess (const 200) $
-    it "demonstrates best practices" $
-      hedgehog $ do
-        response <- ApiProperties.getTestResponse
+apiPropertySpec = before provideRequestHandler $ describe "API best practices" $ do
+  x100 $ do
+    it "is applied to Health API" $
+      requireApiBestPracticesFor (Proxy @HealthAPI)
 
-        alwaysCacheControlOnGetRequests response
-        neverRespondWithInternalError response
+    it "is applied to Voting API" $
+      requireApiBestPracticesFor (Proxy @VotingAPI)
+
+    it "is applied to Comments API" $
+      requireApiBestPracticesFor (Proxy @CommentsAPI)
+
+    it "is applied to the full API as a whole" $
+      -- Yes this is (mostly) a duplicate of previous tests, it's just in case a new
+      -- set of endpoints get added but not added here to the tests.
+      requireApiBestPracticesFor (Proxy @API)
+ where
+  x100 = modifyMaxSuccess (const 100)
 
 exampleHedghehogSpec :: Spec
 exampleHedghehogSpec = describe "Example tests" $ do
@@ -42,6 +51,18 @@ exampleHedghehogSpec = describe "Example tests" $ do
     x :: Int <- forAll $ Gen.integral (Range.linear 0 1000)
     y :: Int <- forAll $ Gen.integral (Range.linear 0 5000)
     liftIO $ threadDelay (10 * x + y)
+
+provideRequestHandler :: IO (Request -> IO SResponse)
+provideRequestHandler = do
+  myApp <- myapi
+  pure $ \req -> do
+    liftIO $ withSession myApp $ do
+      request req
+
+myapi :: IO Application
+myapi = do
+  env <- mkEnv
+  pure $ app env
 
 -- waiHedgehogTesting :: Property
 -- waiHedgehogTesting = Group "API Properties" [
