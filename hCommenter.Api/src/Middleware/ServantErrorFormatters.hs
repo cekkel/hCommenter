5606ef6593a5b9ce46abc2e164fadcfc1e59c57b
Charles Taylor <taylorc822@gmail.com>
2025-03-28 23:07:10 +1100

    feat: add servant response formatters

---
+++ hCommenter.Api/src/Middleware/ServantErrorFormatters.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Middleware.ServantErrorFormatters (customFormatters) where
+
+import Data.Aeson (object, (.=))
+import Network.Wai (Request (rawPathInfo))
+import PyF (fmt)
+import Servant
+  ( ErrorFormatter
+  , ErrorFormatters (bodyParserErrorFormatter, notFoundErrorFormatter)
+  , JSON
+  , NotFoundErrorFormatter
+  , Proxy (Proxy)
+  , ServerError (errBody, errHeaders)
+  , defaultErrorFormatters
+  , err400
+  , err404
+  , getAcceptHeader
+  )
+import Servant.API.ContentTypes (handleAcceptH)
+
+customFormatters :: ErrorFormatters
+customFormatters =
+  defaultErrorFormatters
+    { bodyParserErrorFormatter = customFormatter
+    , notFoundErrorFormatter = notFoundFormatter
+    }
+
+customFormatter :: ErrorFormatter
+customFormatter tr req err =
+  let
+    -- aeson Value which will be sent to the client
+    value = object ["combinator" .= show tr, "error" .= err]
+    -- Accept header of the request
+    accH = getAcceptHeader req
+  in
+    -- handleAcceptH is Servant's function that checks whether the client can accept a
+    -- certain message type.
+    -- In this case we call it with "Proxy '[JSON]" argument, meaning that we want to return a JSON.
+    case handleAcceptH (Proxy :: Proxy '[JSON]) accH value of
+      -- If client can't handle JSON, we just return the body the old way
+      Nothing -> err400 {errBody = fromString err}
+      -- Otherwise, we return the JSON formatted body and set the "Content-Type" header.
+      Just (ctypeH, body) ->
+        err400
+          { errBody = body
+          , errHeaders = [("Content-Type", toStrict ctypeH)]
+          }
+
+notFoundFormatter :: NotFoundErrorFormatter
+notFoundFormatter req =
+  err404 {errBody = [fmt|Not found path: {rawPathInfo req}|]}
