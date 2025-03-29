59b578a7e450a3b194ef8e4cddb7cffd7ef6b0c0
Charles Taylor <taylorc822@gmail.com>
2025-03-29 13:16:28 +1100

    feat: switch to servant-client for logging

--- hCommenter.Api/src/Logging/Grafana.hs
+++ hCommenter.Api/src/Logging/Grafana.hs
@@ -6,10 +6,24 @@
 module Logging.Grafana where
 
 import Data.Aeson
+import Data.Aeson.Encode.Pretty (encodePretty)
 import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
-import Network.HTTP.Req
+import Network.HTTP.Client (Manager, newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Types (statusIsSuccessful)
 import Optics
 import PyF (fmt)
+import Servant (Header, JSON, NoContent (..), Post, Proxy (Proxy), ReqBody, (:>))
+import Servant.Client
+  ( BaseUrl (..)
+  , ClientError
+  , ClientM
+  , ResponseF (responseStatusCode)
+  , Scheme (Https)
+  , client
+  , mkClientEnv
+  , runClientM
+  )
 import System.Environment (getEnv)
 
 import Data.Aeson qualified as A
@@ -22,6 +36,8 @@ data GrafanaConf = GrafanaConf
   { grafanaAccountNum :: !Text
   , grafanaToken :: !Text
   , grafanaUrl :: !Text
+  , httpManager :: !Manager
+  , debugLogs :: !Bool
   }
 
 readGrafanaConf :: IO GrafanaConf
@@ -29,39 +45,66 @@ readGrafanaConf = do
   grafanaAccountNum <- pack <$> getEnv "LOGGING__GRAFANA_ACC"
   grafanaToken <- pack <$> getEnv "LOGGING__GRAFANA_TOKEN"
   grafanaUrl <- pack <$> getEnv "LOGGING__GRAFANA_URL"
+  debugLogs <- (== "true") . toLower <$> getEnv "LOGGING__ENABLE_LOG_DEBUGGING_IN_CONSOLE"
+
+  httpManager <- newManager tlsManagerSettings
 
   pure $ GrafanaConf {..}
 
 makeFieldLabelsNoPrefix ''GrafanaConf
 
+makePrisms ''ClientError
+
 mkGrafanaScribe :: GrafanaConf -> K.PermitFunc -> K.Verbosity -> IO K.Scribe
 mkGrafanaScribe conf permitF verbosity = do
   pure $ K.Scribe {scribePermitItem = permitF, scribeFinalizer = pure (), liPush = pushLog verbosity conf}
 
+type GrafanaAPI =
+  "loki"
+    :> "api"
+    :> "v1"
+    :> "push"
+    :> Header "Authorization" Text
+    :> Header "Content-Type" Text
+    :> ReqBody '[JSON] Value
+    :> Post '[JSON] NoContent
+
+api :: Proxy GrafanaAPI
+api = Proxy
+
+sendLog :: Maybe Text -> Maybe Text -> Value -> ClientM NoContent
+sendLog = client api
+
 pushLog :: forall a. (K.LogItem a) => K.Verbosity -> GrafanaConf -> K.Item a -> IO ()
-pushLog verbosity conf item = runReq defaultHttpConfig $ do
+pushLog verbosity conf item = do
   let
-    options =
-      mconcat
-        [ header "Authorization" [fmt|Bearer {conf ^. #grafanaAccountNum}:{conf ^. #grafanaToken}|]
-        , header "Content-Type" "application/json"
-        ]
+    clientEnv =
+      mkClientEnv
+        (conf ^. #httpManager)
+        ( BaseUrl
+            { baseUrlScheme = Https
+            , baseUrlHost = unpack $ conf ^. #grafanaUrl
+            , baseUrlPath = ""
+            , baseUrlPort = 443
+            }
+        )
 
-  r <-
-    req
-      POST
-      (https (conf ^. #grafanaUrl) /: "loki" /: "api" /: "v1" /: "push")
-      (ReqBodyJson $ encodeLoki verbosity item)
-      ignoreResponse
-      options
-      `catchAny` \e -> do
-        putStrLn "Failed to push log to Grafana:"
-        print e
-        throwIO e
+  let
+    token = Just [fmt|Bearer {conf ^. #grafanaAccountNum}:{conf ^. #grafanaToken}|]
+    contentType = Just "application/json"
+    payload = encodeLoki verbosity item
 
-  when (responseStatusCode r /= 204) $ do
-    putStrLn "Status code indicates failure to push log to Grafana:"
-    print r
+  response <- runClientM (sendLog token contentType payload) clientEnv
+
+  when (conf ^. #debugLogs) $ do
+    putStrLn [fmt|Pushed log to Grafana: {encodePretty payload}|]
+    putStrLn [fmt|Got response from Grafana: {tshow response}|]
+
+  let
+    non200StatusCode = response ^? _Left % _FailureResponse % _2 % to responseStatusCode
+
+  unless (maybe True statusIsSuccessful non200StatusCode) $ do
+    putStrLn [fmt|Error occurred while pushing log to Grafana: {tshow response}|]
 
 utcTimeToEpochTime :: UTCTime -> Int
 utcTimeToEpochTime = round . (* 1e9) . utcTimeToPOSIXSeconds
@@ -70,6 +113,7 @@ encodeLoki :: (K.LogItem a) => K.Verbosity -> K.Item a -> Value
 encodeLoki verbosity item =
   let
     msg = toStrict $ Builder.toLazyText $ K.unLogStr $ K._itemMessage item
+    -- FIXME: This is currently the same time for all logs in a request. Need to verify if correct.
     logTime = tshow $ utcTimeToEpochTime $ K._itemTime item
     environment = K.getEnvironment $ K._itemEnv item
     jsonMsg =
@@ -100,7 +144,8 @@ encodeLoki verbosity item =
 newtype Loki = Loki
   { streams :: [Stream]
   }
-  deriving newtype (Generic, Show, ToJSON)
+  deriving stock (Generic, Show)
+  deriving anyclass (ToJSON)
 
 data Stream = Stream
   { stream :: StreamInfo
