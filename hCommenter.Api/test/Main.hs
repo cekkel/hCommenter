8f6b1cd4bac38e7797a4397ee66c0bb68686394f
Charles Taylor <taylorc822@gmail.com>
2025-03-31 10:08:31 +1100

    fix: init DB during tests

--- hCommenter.Api/test/Main.hs
+++ hCommenter.Api/test/Main.hs
@@ -4,10 +4,13 @@ import System.Environment (setEnv)
 
 import ApiRules.HttpClient (runInstanceRuleTests)
 import ApiRules.Wai
+import Server (initDevSqliteDB, readEnv)
 
 main :: IO ()
 main = do
   setEnv "LOGGING__SEVERITY" "Critical" -- So that almost all logs are hidden, except critical (unexpected) failures
   setEnv "API_ENVIRONMENT" "Development" -- To ensure that logs are written to the console
+  _ <- initDevSqliteDB <$> readEnv
+
   runWaiRuleTests
   runInstanceRuleTests
